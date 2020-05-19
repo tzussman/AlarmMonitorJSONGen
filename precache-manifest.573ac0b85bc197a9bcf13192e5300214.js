f2218e8eaaa7c4548047802d7246514d1825aea1
tzussman <talzuss@gmail.com>
2020-05-19 17:59:31 -0400

    Updates

---
+++ precache-manifest.573ac0b85bc197a9bcf13192e5300214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db2ec3ff0a5b5a70c89a6b7edbf275",
+    "url": "/AlarmMonitorJSONGen/index.html"
+  },
+  {
+    "revision": "fd42cdfd3e505766ab44",
+    "url": "/AlarmMonitorJSONGen/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "1c1c860562f0652175ae",
+    "url": "/AlarmMonitorJSONGen/static/js/2.2b57d44e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AlarmMonitorJSONGen/static/js/2.2b57d44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd42cdfd3e505766ab44",
+    "url": "/AlarmMonitorJSONGen/static/js/main.24763e1d.chunk.js"
+  },
+  {
+    "revision": "c901a3bdbde1f00b8f48",
+    "url": "/AlarmMonitorJSONGen/static/js/runtime-main.1afd2dcc.js"
+  },
+  {
+    "revision": "0f48c6dfd09d3b3d55af6d7c098d22ab",
+    "url": "/AlarmMonitorJSONGen/static/media/Columbia_University_Logo-white.0f48c6df.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AlarmMonitorJSONGen/precache-manifest.1a6c4982cb0ee5d76fdf520ef9c7d571.js"
+  "/AlarmMonitorJSONGen/precache-manifest.573ac0b85bc197a9bcf13192e5300214.js"
 );
 
 self.addEventListener('message', (event) => {
